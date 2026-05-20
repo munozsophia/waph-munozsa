24a52d9ff6a887b4a5458b20b4029924eb5efbb4
Sophia Munoz <munozsa@mail.uc.edu>
2026-05-19 23:38:08 -0400

    add cgi programs

---
+++ labs/lab1/index.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Content-Type: text/html; charset=utf-8\n\n");
+	printf("<!DOCTYPE html>\n");
+	printf("<html>\n");
+	printf("<head>\n");
+	printf("<title>WAPH - Sophia Munoz</title>\n");
+	printf("</head>\n");
+	printf("<body>\n");
+	printf("<h1>Web Application Programming and Hacking</h1>\n");
+	printf("<p>This is a CGI application made by Sophia Munoz.</p>\n");
+	printf("</body>\n");
+	printf("</html>\n");
+	return 0;
+}
