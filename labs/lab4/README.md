ed49212b3d34b973df68e4e630cd90b6e312605a
Sophia Munoz <munozsa@mail.uc.edu>
2026-07-01 20:09:48 -0400

    Copy code from Lab 3 to Lab 4

--- labs/lab4/README.md
+++ labs/lab4/README.md
@@ -24,9 +24,24 @@ Lab4 Folder: [https://github.com/munozsophia/waph-munozsa/tree/main/labs/lab4](h
 
 #### a. Deploy and test sessiontest.php
 
+![Different Session Values from Different Browsers](../../images/sessiontest-php-deploy-browsers.png)
+*Different Session Values from Different Browsers*
+
 #### b. Observe the Session-Handshaking Wireshark
 
+![Wireshark - 1st HTTP Request/Response](../../images/sessiontest-php-wireshark-first-stream.png)
+*Wireshark - 1st HTTP Request/Response*
+
+![Wireshark - After 1st HTTP Request/Response](../../images/sessiontest-php-wireshark-after-first-stream.png)
+*Wireshark - After 1st HTTP Request/Response*
+
 #### c. Understanding Session Hijacking
+
+![Before Session Hijacking](../../images/sessiontest-php-before-hijacking.png)
+*Before Session Hijacking*
+
+![After Session Hijacking](../../images/sessiontest-php-after-hijacking.png)
+*After Session Hijacking*
 
 ### Task 2: Insecure Session Authentication
 
--- labs/lab4/index.php
+++ labs/lab4/index.php
@@ -1,20 +1,15 @@
 <?php
-	session_start();    
-	if (checklogin_mysql($_POST["username"],$_POST["password"])) {
+	session_start();
+	if (isset($_POST["username"]) and isset($_POST["password"])) {   
+		if (checklogin_mysql($_POST["username"],$_POST["password"])) {
 ?>
-	<h2> Welcome <?php echo htmlentities($_POST['username']); ?> !</h2>
+		<h2> Welcome <?php echo htmlentities($_POST['username']); ?> !</h2>
 <?php		
-	}else{
-		echo "<script>alert('Invalid username/password');window.location='form.php';</script>";
-		die();
+		}else{
+			echo "<script>alert('Invalid username/password');window.location='form.php';</script>";
+			die();
+		}
 	}
-	function checklogin($username, $password) {
-		$account = array("admin","1234");
-		if (($username== $account[0]) and ($password == $account[1])) 
-		  return TRUE;
-		else 
-		  return FALSE;
-  	}
   	function checklogin_mysql($username, $password) {
 		$mysqli = new mysqli('localhost','munozsa' /*Database username*/,'Sophia13_m' /*Database password*/,'waph' /*Database name*/);
 		if ($mysqli->connect_errno) {
